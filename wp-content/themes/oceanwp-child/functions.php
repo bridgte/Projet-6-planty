71b18b7bfdbaf3e43f1473cec03f2df0ace1da5c
bridgte <audrey.poissonneau@gmail.com>
2024-02-20 19:28:50 +0100

    debut mise en page formulaire

--- wp-content/themes/oceanwp-child/functions.php
+++ wp-content/themes/oceanwp-child/functions.php
@@ -23,13 +23,13 @@ add_action( 'wp_enqueue_scripts', 'child_theme_configurator_css', 10 );
 
 // END ENQUEUE PARENT ACTION
 
-add_filter('wp_nav_menu_items', 'add_extra_item_to_nav_menu',10,2);
 
 
 function add_extra_item_to_nav_menu($items,$args) {
-    if(is_user_logged_in() && current_user_can('administrator')) {
-        $lien_admin = '<li><a href="'.get_site_url().'/wp-admin'.'">admin</a></li>';
-        $items .= $items_admin;
+    if(is_user_logged_in() && $args->menu == 3) {
+        $items .= '<li class="menu_item"><a href="'.get_site_url().'/wp-admin'.'"> admin</a></li>';
     }
     return $items;
 }
+
+add_filter('wp_nav_menu_items', 'add_extra_item_to_nav_menu',1,2);
